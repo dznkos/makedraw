20504b5c1046aa28bc9dd11c0398253d39ab7a34
Luis Vice <khanccer@gmail.com>
2020-09-16 22:43:30 -0500

    makedraw-6

--- lib/src/pages/principal_page.dart
+++ lib/src/pages/principal_page.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
+import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class PrincipalPage extends StatelessWidget {
 
@@ -11,14 +10,14 @@ class PrincipalPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-            diseno(),
+        children: <Widget>[
+          _fondoApp(),
 
           SingleChildScrollView(
             child: Column(
-              children: [
-                _tituloGrid(),
-                mainGrid(),
+              children: <Widget>[
+                _titulos(),
+                _botonesRedondeados(),
               ],
             ),
           )
@@ -28,21 +27,85 @@ class PrincipalPage extends StatelessWidget {
     );
   }
 
+  Widget _fondoApp(){
+
+    final gradiente = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: FractionalOffset(0.0, 0.6),
+              end: FractionalOffset(0.0, 1.0),
+              colors: [
+                Color.fromRGBO(52, 54, 101, 1.0),
+                Color.fromRGBO(35, 37, 57, 1.0)
+              ]
+          )
+      ),
+    );
+
+
+    final cajaRosa = Transform.rotate(
+        angle: -pi / 5.0,
+        child: Container(
+          height: 360.0,
+          width: 360.0,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(80.0),
+              gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(236, 98, 188, 1.0),
+                    Color.fromRGBO(241, 142, 172, 1.0)
+                  ]
+              )
+          ),
+        )
+    );
+
+    return Stack(
+      children: <Widget>[
+        gradiente,
+        Positioned(
+            top: -100.0,
+            child: cajaRosa
+        )
+      ],
+    );
+
+  }
+
+  Widget _titulos() {
+
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Classify transaction', style: TextStyle( color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold )),
+            SizedBox( height: 10.0 ),
+            Text('Classify this transaction into a particular category', style: TextStyle( color: Colors.white, fontSize: 18.0 )),
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget _bottomNavigationBar(BuildContext context){
 
-    return new Theme(
+    return Theme(
         data: Theme.of(context).copyWith(
-          canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
-          primaryColor: Colors.pinkAccent,
-          textTheme: Theme.of(context).textTheme.copyWith(
-            caption: TextStyle( color: Color.fromRGBO(116, 117, 152, 1.0))
-          )
+            canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+            primaryColor: Colors.pinkAccent,
+            textTheme: Theme.of(context).textTheme.copyWith(
+                caption: TextStyle( color: Color.fromRGBO(116, 117, 152, 1.0))
+            )
         ),
         child: BottomNavigationBar(
             items: [
               BottomNavigationBarItem(
-                  icon: Icon( Icons.calendar_today, size: 30.0,),
-                  title: Container(),
+                icon: Icon( Icons.calendar_today, size: 30.0,),
+                title: Container(),
               ),
               BottomNavigationBarItem(
                 icon: Icon( Icons.bubble_chart, size: 30.0,),
@@ -52,133 +115,75 @@ class PrincipalPage extends StatelessWidget {
                 icon: Icon( Icons.supervised_user_circle, size: 30.0,),
                 title: Container(),
               )
-
             ]
         )
     );
-
   }
 
-  
-  Widget diseno() {
-    
-  final box = Transform.rotate(
-      angle: -pi / 4.5,
-      child: Container(
-          width: 360,
-          height: 360,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(80),
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(252, 97, 255, 1.0),
-                Color.fromRGBO(252, 210, 185, 1.0),
-              ],
-            ),
-          ),
-        ),
-  );
+  Widget _botonesRedondeados() {
 
-  final gradientStyle = Container(
-    width: double.infinity,
-    height: double.infinity,
-    decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromRGBO(52, 54, 101, 1.0),
-            Color.fromRGBO(35, 37, 57, 1.0),
-          ],
-          begin: FractionalOffset(0.0, 0.6),
-          end: FractionalOffset(0.0, 1.0),
-        )
-    ),
-  );
-
-  return Stack(
-      children: <Widget>[
-        gradientStyle,
-        Positioned(
-            top: -100,
-            left: -10,
-            child: box)
-      ],
-  );
-
-  }
-
-
-  Widget mainGrid() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Table(
+    return Table(
             children: [
               TableRow(
-                children: [
-                    _crearBotonGrid(),
-                    _crearBotonGrid()
-                ]
-              ),
-              TableRow(
                   children: [
-                    _crearBotonGrid(),
-                    _crearBotonGrid()
+                    _crearBotonRedondeado( Colors.blue, Icons.border_all, 'General' ),
+                    _crearBotonRedondeado( Colors.purpleAccent, Icons.directions_bus, 'Bus' ),
                   ]
               ),
               TableRow(
                   children: [
-                    _crearBotonGrid(),
-                    _crearBotonGrid()
+                    _crearBotonRedondeado( Colors.pinkAccent, Icons.shop, 'Buy' ),
+                    _crearBotonRedondeado( Colors.orange, Icons.insert_drive_file, 'File' ),
                   ]
               ),
-            ],
-        ),
-    );
-  }
-
-  Widget _crearBotonGrid(){
-    return Container(
-      height: 130,
-      margin: EdgeInsets.all(10.0) ,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(62, 66, 107, 0.8),
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-
-          CircleAvatar(
-            backgroundColor: Colors.pinkAccent,
-            radius: 20,
-            child: Icon( Icons.phone, color: Colors.blue, size: 20,),
-          ),
-          Text('Datos', style: TextStyle( color: Colors.pinkAccent),),
-          SizedBox(height: 1),
-
-        ],
-      ),
-    );
-  }
-
-  Widget _tituloGrid() {
-    return Container(
-      padding: EdgeInsets.only(right: 45, left: 20, bottom: 15 ),
-      child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 60),
-              Text('Clasificacion transacciones', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),),
-              SizedBox(height: 10,),
-              Container(
-                width: 220,
-                //color: Colors.pink,
-                child: Text('Classify thsi trasaction into a particular cateogy',maxLines: 2, style: TextStyle(fontSize: 16, color: Colors.white,),),
+              TableRow(
+                  children: [
+                    _crearBotonRedondeado( Colors.blueAccent, Icons.movie_filter, 'Entertaiment' ),
+                    _crearBotonRedondeado( Colors.green, Icons.cloud, 'Grocery' ),
+                  ]
+              ),
+              TableRow(
+                  children: [
+                    _crearBotonRedondeado( Colors.red, Icons.collections, 'Photos' ),
+                    _crearBotonRedondeado( Colors.teal, Icons.help_outline, 'General' ),
+                  ]
               )
-
             ],
+    );
+  }
+
+  Widget _crearBotonRedondeado( Color color, IconData icono, String texto ) {
+
+
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur( sigmaX: 20.0, sigmaY: 20.0 ),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox( height: 5.0 ),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon( icono, color: Colors.white, size: 30.0 ),
+              ),
+              Text( texto , style: TextStyle( color: color )),
+              SizedBox( height: 5.0 )
+            ],
+          ),
+        ),
       ),
     );
   }
+
+
 
 
 
