4570ff7aa2cb43afdb5d90bf5d3a424646ecd924
Luis Vice <khanccer@gmail.com>
2020-09-16 23:57:57 -0500

    makedraw-7

--- lib/src/pages/principal_page.dart
+++ lib/src/pages/principal_page.dart
@@ -154,32 +154,42 @@ class PrincipalPage extends StatelessWidget {
 
   Widget _crearBotonRedondeado( Color color, IconData icono, String texto ) {
 
+    final _containButton = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        SizedBox( height: 5.0 ),
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 35.0,
+          child: Icon( icono, color: Colors.white, size: 30.0 ),
+        ),
+        Text( texto , style: TextStyle( color: color )),
+        SizedBox( height: 5.0 )
+      ],
+    );
 
-    return ClipRect(
-      child: BackdropFilter(
-        filter: ImageFilter.blur( sigmaX: 20.0, sigmaY: 20.0 ),
-        child: Container(
-          height: 180.0,
-          margin: EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-              color: Color.fromRGBO(62, 66, 107, 0.7),
-              borderRadius: BorderRadius.circular(20.0)
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              SizedBox( height: 5.0 ),
-              CircleAvatar(
-                backgroundColor: color,
-                radius: 35.0,
-                child: Icon( icono, color: Colors.white, size: 30.0 ),
+    return  Stack(
+      children:[
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaY: 10.0,sigmaX: 10.0) ,
+              child: Container(
+                  height: 180.0,
+                  width: 170,
+                  //margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(62, 66, 107, 0.7),
+                      borderRadius: BorderRadius.circular(20.0)
+                  ),
+                  child: _containButton
               ),
-              Text( texto , style: TextStyle( color: color )),
-              SizedBox( height: 5.0 )
-            ],
+            ),
           ),
         ),
-      ),
+      ]
     );
   }
 
